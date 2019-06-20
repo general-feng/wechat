cac65dd4c3b74de692e7a7d81c79c437b813e0dc
penglh <penglh@wangsu.com>
2019-06-20 10:13:40 +0800

    个人信息页面加canvas

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -18,6 +18,12 @@ Page({
     })
   },
   /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function (e) {
+    this.drawCanvas()
+  },
+  /**
      * 生命周期函数--监听页面显示
      */
   onShow() {
@@ -27,5 +33,26 @@ Page({
         selected: 1
       })
     }
+  },
+  drawCanvas: function (e) {
+    var ctx = wx.createCanvasContext('myCanvas')
+    const grd = ctx.createLinearGradient(0, 0, 400, 0)
+    grd.addColorStop(0, 'white')
+    grd.addColorStop(0.5, '#5095f7')
+    grd.addColorStop(1, 'white')
+    ctx.setFillStyle(grd)
+    ctx.fillRect(0, 0, 400, 2)
+    var txt="如果不是终点，请微笑向前！"
+    ctx.setFillStyle('#5095f7');
+    ctx.setFontSize(16);
+    ctx.setTextAlign('center');
+    ctx.fillText(txt, 200, 32)
+    const grdE = ctx.createLinearGradient(0, 60, 400, 60)
+    grdE.addColorStop(0, 'white')
+    grdE.addColorStop(0.5, '#5095f7')
+    grdE.addColorStop(1, 'white')
+    ctx.setFillStyle(grdE)
+    ctx.fillRect(0, 50, 400, 2)
+    ctx.draw()
   }
 })
